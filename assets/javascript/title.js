f7de0fb012c6c7178a9e90747cd125a3e74f1aa9
dzired <70129751+dzired@users.noreply.github.com>
2021-07-05 15:15:33 -0700

    Add files via upload

---
+++ assets/javascript/title.js
@@ -0,0 +1,9 @@
+var i=0;
+setInterval(function(){
+    var titles=['mehf', 'mehf on', 'mehf on top', 'love yall <3', 'have a', 'good day'];//add more titles if you want
+    if(i===titles.length) {
+        i=0;
+    }
+    document.title = titles[i];
+    i++;
+}, 1500);
