7d7d027200057ce260a5592398a3699f4a965a44
dzired <70129751+dzired@users.noreply.github.com>
2021-07-05 22:40:33 -0700

    Update title.js

--- assets/javascript/title.js
+++ assets/javascript/title.js
@@ -1,9 +1,9 @@
 var i=0;
 setInterval(function(){
-    var titles=['[    ]', '[=   ]', '[==  ]', '[=== ]', '[ ===]', '[  ==]', '[   =]', '[    ]', '[   =]', '[  ==]', '[ ===]', '[====]', '[=== ]', '[==  ]', '[=   ]'];//add more titles if you want
+    var titles=['💛', '💙', '💜', '💚', '❤️' , 'mehf', 'mehf', 'mehf on', 'mehf on', 'mehf on top', 'm', 'me', 'meh', 'mehf', 'mehf o', 'mehf on', 'mehf on t', 'mehf on to', 'mehf on top', '💛', '💙', '💜', '💚', '❤️'];//add more titles if you want
     if(i===titles.length) {
         i=0;
     }
     document.title = titles[i];
     i++;
-}, 300);
+}, 500);
