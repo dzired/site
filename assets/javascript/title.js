bd01ed9854bca5f856ba17d434b8ab5d7edccbe7
dzired <70129751+dzired@users.noreply.github.com>
2021-07-05 22:08:04 -0700

    Update title.js

--- assets/javascript/title.js
+++ assets/javascript/title.js
@@ -1,9 +1,9 @@
 var i=0;
 setInterval(function(){
-    var titles=['💙', '💜', '💚', '❤️', '💛'];//add more titles if you want
+    var titles=["[    ]", "[=   ]", "[==  ]", "[=== ]", "[ ===]", "[  ==]", "[   =]", "[    ]", "[   =]", "[  ==]", "[ ===]", "[====]", "[=== ]", "[==  ]", "[=   ]"];//add more titles if you want
     if(i===titles.length) {
         i=0;
     }
     document.title = titles[i];
     i++;
-}, 200);
+}, 300);
