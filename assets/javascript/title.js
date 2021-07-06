dafaf0846321b2f44c2de7868d9bf40087ccdc13
dzired <70129751+dzired@users.noreply.github.com>
2021-07-05 21:21:07 -0700

    Update title.js

--- assets/javascript/title.js
+++ assets/javascript/title.js
@@ -1,9 +1,9 @@
 var i=0;
 setInterval(function(){
-    var titles=['mehf', 'mehf on', 'mehf on top', 'love yall <3', 'have a', 'good day'];//add more titles if you want
+    var titles=['▖m', '▘m', '▝me', '▗me', '▖meh', '▘meh', '▝mehf', '▗mehf', '▖mehf o', '▘mehf o', '▝mehf on', '▗mehf on','▖mehf on t', '▘mehf on t', '▝mehf on to', '▗mehf on to', '▖mehf on top', '▘mehf on top', 'have a', 'have a', 'have a', 'great day', 'great day', 'great day', '💛', '💛', '💙', '💙', '💜', '💜'];//add more titles if you want
     if(i===titles.length) {
         i=0;
     }
     document.title = titles[i];
     i++;
-}, 2000);
+}, 400);
