279259df742bc34bfbf30ac3d8134042da90b76c
dzired <70129751+dzired@users.noreply.github.com>
2021-07-07 18:06:16 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,46 @@
-var audio = new Audio("audio.mp3");
-  audio.volume = 0.2;
-  audio.loop = true;
-document.onclick = function() {
-  audio.play();
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.12;
+  musicFadeIn = 100;
+
+
+
+  titleChanger = (text, delay) => {
+    if (!text) return;
+
+    delay = delay || 2000;
+
+    let counter = 0;
+
+    setInterval(() => {
+      if (counter < text.length) document.title = text[counter++];
+      else document.title = text[(counter = 0)];
+    }, delay);
+  };
+
+  iconChanger = (urls, delay) => {
+    if (!urls) return;
+
+    delay = delay || 200;
+
+    let counter = 0;
+
+    setInterval(() => {
+      if (counter < urls.length) {
+        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = urls[counter];
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+      } else counter = 0;
+
+      ++counter;
+    }, delay);
+  };
 }
+
+const app = new _app();
