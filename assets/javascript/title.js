f43efa3e4c14a38fcc8d6dbf940fb9d796bc5830
dzired <70129751+dzired@users.noreply.github.com>
2021-07-07 22:03:51 -0700

    Update title.js

--- assets/javascript/title.js
+++ assets/javascript/title.js
@@ -1,9 +1,36 @@
 var i=0;
 setInterval(function(){
-    var titles=['💙', '💜', '💚', '❤️' , '💛', 'mehf', 'mehf on', 'mehf on top', 'm', 'me', 'meh', 'mehf', 'mehf o', 'mehf on', 'mehf on t', 'mehf on to', 'mehf on top', '💙', '💜', '💚', '❤️', '💛', '◢','◣','◤','◥', '◢','◣','◤','◥'];//add more titles if you want
+    var titles=[
+"||",
+"|m|",
+"|me|",
+"|meh|",
+"|mehf|",
+"|mehf o|",
+"|mehf on|",
+"|mehf on |",
+"|mehf on t|",
+"|mehf on to|",
+"|mehf on top|",
+"|mehf on top|",
+"|mehf on top|",
+" |mehf on to|",
+"  |mehf on t|",
+"   |mehf on |",
+"    |mehf on|",
+"     |mehf o|",
+"      |mehf |",
+"       |mehf|",
+"        |meh|",
+"         |me|",
+"          |m|",
+"           ||",
+
+]
+
     if(i===titles.length) {
         i=0;
     }
     document.title = titles[i];
     i++;
-}, 350);
+}, 900);
