fb060b38e53e39520494a409a9ac312f590485b1
dzired <70129751+dzired@users.noreply.github.com>
2021-07-28 09:23:38 -0700

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -23,12 +23,3 @@ e.preventDefault();
 });
 
 
-
-$(document).ready(function(){
-var screenWidth = $(window).width();
-if (screenWidth < 800){
-    $('video').removeAttr('autoplay');
-} else {
-$('video').attr('autoplay');
-}
-});
