ef61b2e39af8648fff0cf797daeee16727a3a290
dzired <70129751+dzired@users.noreply.github.com>
2021-07-30 14:44:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 
-//Video volume
-document.getElementById("vid").volume = 0.18;
 
 
 //Main Function
 mainActive = false;
-setTimeout(() => {if (document.getElementById("vid").paused) {$("#clickToPlay").css("opacity", "1");} else {main()}}, 1000);
+
 function main() {
     if (mainActive) return;
     mainActive = true;
-    $("#clickToPlay").css("opacity", "0");
+ 
     $("#main").css("opacity", "1");
     $("#widgetContainer").css("opacity", "1");
     $(".hitCount").css("opacity", "1");
-    setTimeout(() => {
-        $("#clickToPlay").css("display", "none");
-    }, 1050);
-    document.getElementById("vid").play();
+
+
     //Border
     borderTop = 0;
     borderRight = 0;
@@ -120,8 +116,3 @@ function main() {
     topBorder();
 }
 
-
-//Start Main Function
-$("#clickToPlay").click(() => {
-    main();
-});
