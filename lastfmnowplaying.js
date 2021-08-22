84dac359ed7e9a85435a55461607273a2b7dd4c1
dzired <70129751+dzired@users.noreply.github.com>
2021-08-21 21:55:39 -0700

    Update lastfmnowplaying.js

--- lastfmnowplaying.js
+++ lastfmnowplaying.js
@@ -1,55 +1,38 @@
+// Hacky last.fm Now Playing
+// by Zack Fernandes (zackfern.me) April 2013
+//
+// Instructions:
+// - Install jQuery, if you don't have it already.
+// - Create a last.fm API key and set the api_key variable.
+// - Set the username variable to your last.fm username.
+// - Add something with the ID of now_playing in your site.
+// - Include this script and enjoy.
+//
+// last.fm API documentation for the method we're using:
+// http://www.last.fm/api/show/user.getRecentTracks
 
-var lastfmData = {
-  baseURL:
-    "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=",
-  // Your Last.fm Username
-  user: "mehf",
-  // Your API key
-  api_key: "9d93c9ed1ec5d370bed6a768664de7c5",
-  additional: "&format=json&limit=1"
-};
-
-var getSetLastFM = function() {
-  $.ajax({
-    type: "GET",
-    url:
-      lastfmData.baseURL +
-      lastfmData.user +
-      "&api_key=" +
-      lastfmData.api_key +
-      lastfmData.additional,
-    dataType: "json",
-    success: function(resp) {
-      var recentTrack = resp.recenttracks.track[0];
-      var formatted = recentTrack.name;
-      $("a#tracktitle")
-        .html(formatted)
-        .attr("href", recentTrack.url)
-        .attr("title", recentTrack.name + " by " + recentTrack.artist["#text"])
-        .attr("target", "_blank");
-
-      var artistFormatted =
-        recentTrack.artist["#text"];
-      $("a#trackartist")
-        .html(artistFormatted)
-        .attr("title", "Artist : " + recentTrack.artist["#text"]);
-      $("img#trackart").attr("src", recentTrack.image[2]["#text"]);
-    },
-    error: function(resp) {
-      $("a#tracktitle").html(
-        "<img src='https://i.imgur.com/EgWjJry.png'>" + "Silence!"
-      );
-      $("img#trackart").attr("src", "https://i.imgur.com/Q6cCswP.jpg");
-      var artistFormatted =
-        "<img src='https://i.imgur.com/fae5XZA.png'>Prashant Shrestha";
-      $("a#trackartist")
-        .html(artistFormatted)
-        .attr("href", "www.prashant.me/");
+$(function(){
+  // Create a new API Key here: http://www.last.fm/api/account/create
+  var api_key = '9d93c9ed1ec5d370bed6a768664de7c5';
+  // Your last.fm username. We'll use this to fetch your tracks and link to your profile.
+  var username = 'mehf';
+  // This is the URL of the API we're calling.
+  var base_url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+username+'&api_key='+api_key+'&limit=1&format=json';
+  $.getJSON(base_url, function(data){
+    // Check to see if we're getting an Array or Object.
+    // If we're listening to something right now, the API returns the song we're currently listening to as well as the last song we listened to, which would be an Array.
+    // If we aren't listening toa nything right now, the API returns the last song we listened to, which would appear to jQuery as an Object.
+    if(jQuery.type(data.recenttracks.track) === 'array') {
+      // Grab the first item out of the Array.
+      var nowPlaying = data.recenttracks.track[0];
+      var listening_text = 'Currently listening to ';
     }
+    else {
+      // We don't have an Array, so just return the whole Hash.
+      var nowPlaying = data.recenttracks.track;
+      var listening_text = 'Last listened to ';
+    }
+    var currentTrack = nowPlaying['name'] + ' by ' + nowPlaying['artist']['#text'];
+    $('#now_playing').html('<a href="http://last.fm/user/' + username + '">'+ listening_text + currentTrack + '</a>');
   });
-};
-
-// Get the new one.
-getSetLastFM();
-// Start the countdown.
-setInterval(getSetLastFM, 10 * 1000);
+});
