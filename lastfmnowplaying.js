ee5900f2159b1a1727c97aaab9794423dd9dfc7a
dzired <70129751+dzired@users.noreply.github.com>
2022-02-24 20:12:14 -0800

    Update lastfmnowplaying.js

--- lastfmnowplaying.js
+++ lastfmnowplaying.js
@@ -1,49 +1,31 @@
-
-
-// by Zack Fernandes (zackfern.me) April 2013
-//
-// Instructions:
-// - Install jQuery, if you don't have it already.
-// - Create a last.fm API key and set the api_key variable.
-// - Set the username variable to your last.fm username.
-// - Add something with the ID of now_playing in your site.
-// - Include this script and enjoy.
-//
-// last.fm API documentation for the method we're using:
-// http://www.last.fm/api/show/user.getRecentTracks
-
 var gamer = function(){
-  // Create a new API Key here: http://www.last.fm/api/account/create
-  var api_key = '9d93c9ed1ec5d370bed6a768664de7c5';
-  // Your last.fm username. We'll use this to fetch your tracks and link to your profile.
-  var username = 'mehf';
-  // This is the URL of the API we're calling.
-  var base_url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+username+'&api_key='+api_key+'&limit=1&format=json';
-  $.getJSON(base_url, function(data){
-    // Check to see if we're getting an Array or Object.
-    // If we're listening to something right now, the API returns the song we're currently listening to as well as the last song we listened to, which would be an Array.
-    // If we aren't listening toa nything right now, the API returns the last song we listened to, which would appear to jQuery as an Object.
-    if(jQuery.type(data.recenttracks.track) === 'array'){
-      // Grab the first item out of the Array.
-      var nowPlaying = data.recenttracks.track[0];
-    } else {
-      // We don't have an Array, so just return the whole Hash.
-      var nowPlaying = data.recenttracks.track;
 
+  var user_id = '852033849567477760';
+  var base_url = 'https://api.lanyard.rest/v1/users/' +user_id
+
+  $.getJSON(base_url, function(data) {
+    var x = data.data.spotify
+    var isListening = data.data.listening_to_spotify
+    if (isListening) {
+      var Song = x.song
+      var Artist = x.artist
+      var cover = x.album_art_url
+      var Artists = Artist.replace(/;/g, ",");
+      var listening_text = "Currently listening to " + Song + " by " + Artists
       
-        
-    }
-      if (nowPlaying['@attr'] && nowPlaying['@attr']['nowplaying']) {
-      var listening_text = 'Currently listening to '; 
-      } else {
-          var listening_text = 'Last listened to ';
-    }
-    var currentTrack = nowPlaying['name'] + ' by ' + nowPlaying['artist']['#text'];
-    $('#now_playing').html('<a href= '+ nowPlaying.url +'>'+ listening_text + currentTrack + '</a>');
+       var imgsrc = cover
+    } else{
+        var listening_text = 'Not listening to anything on Spotify'
+        var imgsrc = "https://i.pinimg.com/originals/f1/f9/fe/f1f9fe2328d1f3b2ff978d4fc67b1a20.gif"
+      }
 
-  });
+    $('#now_playing').html('<a>'+ listening_text + '</a>');
+	$('#album_cover').html('<div id="image"><img src='+imgsrc+'></div>');
+	$('#activity').html('<div class = "brandon"><a>Currently playing: ' + data.data.activities[1].name + '</a></div>')
+
+
+  })
 };
-
 
 gamer();
 setInterval(gamer, 10 * 1000)
