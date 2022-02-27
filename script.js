3b05d0a39e5964b5b597180fb388e0cc06bc33a9
dzired <70129751+dzired@users.noreply.github.com>
2022-02-26 22:19:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const countEl= document.getElementById('count');
 
-$(window).load(function () {
-            $(function () {
-                if (!sessionStorage.getItem("runOnce")) {
+function setCookie(c_name,value,exdays){var exdate=new Date();exdate.setDate(exdate.getDate() + exdays);var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());document.cookie=c_name + "=" + c_value;}
 
-                    updateVisitCount()
+function getCookie(c_name){var c_value = document.cookie;var c_start = c_value.indexOf(" " + c_name + "=");if (c_start == -1){c_start = c_value.indexOf(c_name + "=");}if (c_start == -1){c_value = null;}else{c_start = c_value.indexOf("=", c_start) + 1;var c_end = c_value.indexOf(";", c_start);if (c_end == -1){c_end = c_value.length;}c_value = unescape(c_value.substring(c_start,c_end));}return c_value;}
 
-                    sessionStorage.setItem("runOnce", true);
-                } else {
-                            sameVisitCount()
-                }
-            });
-        });
 
+
+function setCookie(c_name,value,exdays){var exdate=new Date();exdate.setDate(exdate.getDate() + exdays);var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());document.cookie=c_name + "=" + c_value;}
+
+function getCookie(c_name){var c_value = document.cookie;var c_start = c_value.indexOf(" " + c_name + "=");if (c_start == -1){c_start = c_value.indexOf(c_name + "=");}if (c_start == -1){c_value = null;}else{c_start = c_value.indexOf("=", c_start) + 1;var c_end = c_value.indexOf(";", c_start);if (c_end == -1){c_end = c_value.length;}c_value = unescape(c_value.substring(c_start,c_end));}return c_value;}
+
+checkSession();
+
+function checkSession(){
+   var c = getCookie("visited");
+   if (c === "yes") {
+     sameVisitCount()
+   } else {
+     updateVisitCount()
+   }
+   setCookie("visited", "yes", 365); // expire in 1 year; or use null to never expire
+}
 
 
 function updateVisitCount() {
