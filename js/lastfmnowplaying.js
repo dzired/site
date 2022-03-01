9db75441f319aae1b4566335604235726d336788
dzired <70129751+dzired@users.noreply.github.com>
2022-03-01 11:54:46 -0800

    Update lastfmnowplaying.js

--- js/lastfmnowplaying.js
+++ js/lastfmnowplaying.js
@@ -1,5 +1,16 @@
-
-
+function yess() {
+    
+  $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mehf&api_key=9d93c9ed1ec5d370bed6a768664de7c5&limit=1&format=json', function(data){
+    if(jQuery.type(data.recenttracks.track) === 'array'){
+      var nowPlaying = data.recenttracks.track[0];
+    } else {
+      var nowPlaying = data.recenttracks.track;
+    }
+      window.playing = nowPlaying['name']
+      window.artist = nowPlaying['artist']['#text']
+      window.link = nowPlaying.url
+  })
+}
 
 var gamer = function(){
   var api_key = '9d93c9ed1ec5d370bed6a768664de7c5';
@@ -20,12 +31,30 @@ var gamer = function(){
     }
     var currentTrack = nowPlaying['name'] + ' by ' + nowPlaying['artist']['#text'];
     
-    $('#now_playing').html('<a href= '+ nowPlaying.url +'>'+ listening_text + currentTrack + '</a>');
-      $('#album_cover').html('<div id="image"><img src='+cover+'></div>')
+    
+      if (window.playing !== nowPlaying['name']){
+          
+          $('#now_playing').html('<a class = "animate__animated animate__backOutLeft"href= '+ window.link +'>'+ listening_text + window.playing + ' by ' + window.artist + '</a>');
+          var delayInMilliseconds = 1000; //1 second
+          setTimeout(function() {
+          $('#now_playing').html('<a class = "animate__animated animate__backInRight" href= '+ nowPlaying.url +'>'+ listening_text + currentTrack + '</a>');    }, delayInMilliseconds);
+          yess()
+      } else {
+          
+          $('#now_playing').html('<a href= '+ nowPlaying.url +'>'+ listening_text + currentTrack + '</a>');
+      }          
+    $('#trackname').html(nowPlaying['name'])
+    $('#artist').html(nowPlaying['artist']['#text'])
+    $('#isListening').html(listening_text) 
+      
+  })
+    
+  };
 
-  });
-};
 
 
+
+yess()
 gamer();
 setInterval(gamer, 10 * 1000)
+
