e893a4aa3bdd5a8e09402ea62cd6db086d4b4f5e
dzired <70129751+dzired@users.noreply.github.com>
2022-06-11 18:15:35 -0700

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,13 +8,13 @@ $(window).on('keydown',function(event)
     else if(event.ctrlKey && event.shiftKey && event.keyCode==73)
     {
 
-        console.log('%cHi there', 'font-size: 40px; color: green');
+        console.log('%cHi there', 'font-size: 40px; color: white');
 
     }
     else if(event.ctrlKey && event.keyCode==73)
     {
 
-        console.log('%cHi there', 'font-size: 40px; color: green');
+        console.log('%cHi there', 'font-size: 40px; color: white');
 
     }
 });
